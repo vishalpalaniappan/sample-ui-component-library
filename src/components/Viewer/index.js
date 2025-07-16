93d7ec7cc8356e81883ddf14d4167dafd88a6dc3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-16 09:52:52 -0400

    Remove default exports for now.

--- src/components/Viewer/index.js
+++ src/components/Viewer/index.js
@@ -1,2 +1,1 @@
-export { default } from "./Viewer.jsx";
 export * from "./Viewer.jsx"
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-export { default as StackList } from "./components/StackList";
-export { default as Viewer }    from "./components/Viewer";
 export * from "./components/StackList";
 export * from "./components/Viewer";
