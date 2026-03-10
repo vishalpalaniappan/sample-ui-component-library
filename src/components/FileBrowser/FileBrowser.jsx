2741512dcf09150855dd315d43c46c7283a8c045
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-10 07:44:38 -0400

    Add functionality to select nodes.

--- src/components/FileBrowser/FileBrowser.jsx
+++ src/components/FileBrowser/FileBrowser.jsx
@@ -6,10 +6,9 @@ import PropTypes from 'prop-types';
 const INDENT_WIDTH = 20;
 
 
-const TreeNode = ({node}) => {
-    console.log(node.name);
+const TreeNode = ({node, onRowClick}) => {
     return (
-        <div className="node-row">
+        <div className="node-row" onClick={() => onRowClick(node)}>
             <div className="indent" style={{ width: node.level * INDENT_WIDTH + "px"}} />
             {node.name}
         </div>
@@ -21,26 +20,33 @@ const TreeNode = ({node}) => {
  * 
  * @return {JSX}
  */
-export const FileBrowser = ({}) => {
+export const FileBrowser = ({onFileSelect}) => {
 
     const [nodes, setNodes] = useState([]);
 
+    function handleFileClick(node) {
+        console.log("Selected file:", node.name);
+        if (onFileSelect) {
+            onFileSelect(node);
+        }
+    }
+
     const tree = [
-        {"level": 0, "name": "root", "type": "folder"},
-        {"level": 1, "name": "folder1", "type": "folder"},
-        {"level": 2, "name": "file1.txt", "type": "file"},
-        {"level": 2, "name": "file2.txt", "type": "file"},
-        {"level": 1, "name": "folder2", "type": "folder"},
-        {"level": 2, "name": "file3.txt", "type": "file"},
-        {"level": 0, "name": "file4.txt", "type": "file"},
-        {"level": 0, "name": "folder3", "type": "folder"},
-        {"level": 1, "name": "file5.txt", "type": "file"},
+        {"id": 1, "level": 0, "name": "root", "type": "folder"},
+        {"id": 2, "level": 1, "name": "folder1", "type": "folder"},
+        {"id": 3, "level": 2, "name": "file1.txt", "type": "file"},
+        {"id": 4, "level": 2, "name": "file2.txt", "type": "file"},
+        {"id": 5, "level": 1, "name": "folder2", "type": "folder"},
+        {"id": 6, "level": 2, "name": "file3.txt", "type": "file"},
+        {"id": 7, "level": 0, "name": "file4.txt", "type": "file"},
+        {"id": 8, "level": 0, "name": "folder3", "type": "folder"},
+        {"id": 9, "level": 1, "name": "file5.txt", "type": "file"},
     ];
 
     useEffect(() => {
         const rows = [];
         tree.forEach((node) => {
-            rows.push(<TreeNode node={node} />);
+            rows.push(<TreeNode node={node} onRowClick={handleFileClick}/>);
         });
         setNodes(rows);
     }, []);
