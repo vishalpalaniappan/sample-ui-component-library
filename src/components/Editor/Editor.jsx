3f95f2876f7d68cc7e60972edfddd3c2521e7b7f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-20 10:00:41 -0400

    Modify editor component to use reducer.

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -9,11 +9,13 @@ import React, {
     useCallback,
     useImperativeHandle,
     useMemo,
-    useState,
-    useEffect,
+    useReducer,
+    useContext
 } from "react";
 
 import { EditorContext } from "./EditorContext";
+
+import { editorReducer, initialState } from "./EditorReducer";
 
 /**
  * Renders the editor component with support for tabs.
@@ -22,84 +24,45 @@ import { EditorContext } from "./EditorContext";
  */
 export const Editor = forwardRef(({ }, ref) => {
     // TODO: Use reducer to improve state management.
-    const [activeTab, setActiveTab] = useState();
-    const [tabGroupId, setTabGroupId] = useState();
-    const [tabs, setTabs] = useState([]);
-
-    useEffect(() => {
-        // Default to the last tab if no active tab is set.
-        if (tabs.length > 0 && !activeTab) {
-            setActiveTab(tabs[tabs.length - 1]);
-        }
-    }, [tabs]);
+    const [state, dispatch] = useReducer(editorReducer, initialState);
 
     const selectTab = useCallback((id) => {
-        // Select a tab by its id (if the tab exists).
-        if (tabs.length > 0) {
-            const tab = tabs.find(obj => obj.id === id);
-            if (tab) {
-                setActiveTab(tab);
-            } else {
-                console.error(`Tab with id ${id} not found.`);
-            }
-        }
-    }, [tabs]);
+        dispatch({ type: "SELECT_TAB", payload: id });
+    }, []);
 
     const closeTab = useCallback((id) => {
-        // Close a tab by its id (if the tab exists) and update the active tab if necessary.
-        const ind = tabs.findIndex(obj => obj.id === id);
-        if (ind !== -1) {
-            const newTabs = [...tabs];
-            newTabs.splice(ind, 1);
-            setTabs(newTabs);
-
-            // If the closed tab is the active tab, set the active tab to the next available tab.
-            setActiveTab(prev => {
-                return (prev.id === id) ? ((newTabs.length > 0) ? newTabs[Math.max(0, ind - 1)] : null) : prev;
-            });
-        } else {
-            console.warn(`Tab with id ${id} not found.`);
-        }
-    }, [tabs, setActiveTab]);
+        dispatch({ type: "CLOSE_TAB", payload: id });
+    }, []);
 
     const moveTab = useCallback((tabId, newIndex) => {
-        // Move a tab to a new index within the same tab group.
-        const prevTabs = [...tabs];
-        const oldIndex = prevTabs.findIndex(t => t.id === tabId);
-        if (newIndex - 1 === oldIndex || newIndex === oldIndex) return;
-
-        // Remove the tab from its old position and insert it into the new position.
-        const [tab] = prevTabs.splice(oldIndex, 1);
-        newIndex = (oldIndex < newIndex) ? newIndex - 1 : newIndex;
-        prevTabs.splice(newIndex, 0, tab);
-
-        setTabs(prevTabs);
-    }, [tabs, setTabs]);
+        dispatch({ type: "MOVE_TAB", payload: { tabId, newIndex } });
+    }, []);
 
     const addTab = useCallback((tab) => {
-        // Add a new tab to the end of the tabs array.
-        setTabs(prev => [...prev, tab]);
-    }, [tabs, setTabs]);
+        dispatch({ type: "ADD_TAB", payload: tab });
+    }, []);
+
+    const setTabGroupId = useCallback((id) => {
+        dispatch({ type: "SET_PARENT_TAB_GROUP_ID", payload: id });
+    }, []);
 
     const getPreviewElement = useCallback((tabId) => {
         // Get the preview element for a tab by its id for use in drag-and-drop operations.
-        const tab = tabs.find(t => t.id === tabId);
+        const tab = state.tabs.find(t => t.id === tabId);
         return <TabPreview info={{ label: tab.label }} />;
-    }, [tabs]);
+    }, [state]);
 
     const api = useMemo(() => {
         return {
+            state,
             addTab,
-            tabs,
             setTabGroupId,
-            tabGroupId,
             selectTab,
             closeTab,
-            activeTab,
             moveTab,
             getPreviewElement
         };
-    }, [addTab, tabs, tabGroupId, selectTab, closeTab, activeTab, moveTab, getPreviewElement, setTabGroupId]);
+    }, [state, addTab, selectTab, closeTab, moveTab, getPreviewElement, setTabGroupId]);
 
     useImperativeHandle(ref, () => api, [api]);
 
@@ -120,4 +83,12 @@ export const Editor = forwardRef(({ }, ref) => {
 Editor.propTypes = {
     systemTree: PropTypes.object,
     onFileSelect: PropTypes.func
+}
+
+export function useEditor() {
+  const ctx = useContext(EditorContext);
+  if (!ctx) {
+    throw new Error("useEditor must be used inside <Editor>");
+  }
+  return ctx;
 }
