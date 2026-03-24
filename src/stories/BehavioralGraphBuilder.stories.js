0ac39ef31cd35108bca0a2432d5ade5d8d11c379
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-24 18:57:48 -0400

    Add react flow component with tool kit for adding, moving, connecting and deleting nodes.

--- src/stories/BehavioralGraphBuilder.stories.js
+++ src/stories/BehavioralGraphBuilder.stories.js
@@ -1,5 +1,8 @@
+import { useCallback, useState, useEffect } from "react";
 import { BehavioralGraphBuilder } from "../components/BehavioralGraphBuilder";
 import "./BehavioralGraphBuilderStories.scss"
+import { ToolBar } from "./components/ToolBar/ToolBar";
+import { useArgs } from "@storybook/preview-api";
 
 export default {
     title: 'BehavioralGraphBuilder', 
@@ -9,10 +12,26 @@ export default {
 };
 
 const Template = (args) => {
+    const [, updateArgs] = useArgs();
+
+    const [activeTool, setActiveTool] = useState("select");
+
+    const selectTool = useCallback((tool) => {
+        updateArgs({activeTool : tool});
+    }, [activeTool, setActiveTool]);
+    
+    useEffect(() => {
+        updateArgs({activeTool : activeTool});
+    }, []);
 
     return (
         <div className="graphBuilderRootContainer">
-            <BehavioralGraphBuilder  {...args}/> 
+            <div className="toolbar">
+                <ToolBar onSelectTool={selectTool}/>
+            </div>
+            <div className="flow">
+                <BehavioralGraphBuilder {...args}/> 
+            </div>
         </div>
     )
 }
---
+++ src/stories/components/ToolBar/ToolBar.js
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+
+import {
+  Cursor,
+  HandThumbsUp,
+  Square,
+  NodePlus,
+  Trash
+} from "react-bootstrap-icons";
+
+import "./ToolBar.scss";
+
+ToolBar.propTypes = {};
+
+/**
+ * Toolbar Component
+ * @return {JSX.Element}
+ */
+export function ToolBar({ onSelectTool }) {
+  const [selectedTool, setSelectedTool] = useState("select");
+
+  const selectTool = (tool) => {
+    setSelectedTool(tool);
+    onSelectTool(tool);
+  };
+
+  return (
+    <div className="toolbarWrapper">
+      <div className="toolbarContainer">
+        <Cursor
+          onClick={(e) => selectTool("select")}
+          style={{color: selectedTool === "select" ? "white": "grey"}}
+          title="Select"
+          className="icon"
+        />
+        <HandThumbsUp
+          onClick={(e) => selectTool("pan")}
+          style={{color: selectedTool === "pan" ? "white": "grey"}}
+          title="Pan Tool"
+          className="icon"
+        />
+        <Square
+          onClick={(e) => selectTool("drop")}
+          style={{color: selectedTool === "drop" ? "white": "grey"}}
+          title="Add Node"
+          className="icon"
+        />
+        <NodePlus
+          onClick={(e) => selectTool("connect")}
+          style={{color: selectedTool === "connect" ? "white": "grey"}}
+          title="Connect Node"
+          className="icon"
+        />
+        <Trash
+          onClick={(e) => selectTool("delete")}
+          style={{color: selectedTool === "delete" ? "white": "grey"}}
+          title="Delete Node"
+          className="icon"
+        />
+      </div>
+      <div className="toolbarContainer bottom"></div>
+    </div>
+  );
+}
