4e8f9abc00281d15e54e3a2601a8a6c148be2dd4
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-25 18:02:52 -0400

    Remove temporary reference.

--- src/components/BehavioralGraphBuilder/helper.js
+++ src/components/BehavioralGraphBuilder/helper.js
@@ -1,44 +1,5 @@
 export const designToReactFlowElements = (design) => {
 
-    /*
-        Temporary reference:
-
-        Flow Node
-        {
-            id: '1',
-            type: 'input',
-            data: { label: 'Start here...' },
-            position: { x: -150, y: 0 },
-        }
-
-        Flow Edge
-        { id: '1->3', source: '1', target: '3' },
-            
-        Design Node
-        {
-            "uid": "08a4e18c-f293-4ba1-be0a-c8451b50e819",
-            "type": 6,
-            "goToBehaviors": [
-                {
-                    "uid": "35a2759b-e0df-47db-93b7-1f18c216922d",
-                    "type": 1,
-                    "participants": [],
-                    "abstractionIds": [],
-                    "invalidWorldState": false,
-                    "name": "AcceptBookFromUser"
-                }
-            ],
-            "behavior": {
-                "uid": "a873c4ba-3a63-4097-80df-46ed361f2e26",
-                "type": 1,
-                "participants": [],
-                "abstractionIds": [],
-                "invalidWorldState": false,
-                "name": "AcceptChoiceToAddBookToBasket"
-            }
-        }
-     */
-
     let edges = [];
     let nodes = [];
     design.nodes.forEach((node) => {
@@ -52,14 +13,12 @@ export const designToReactFlowElements = (design) => {
         node.goToBehaviors.forEach((goTo) => {
             edges.push({
                 id: `${node.behavior.uid}->${goTo.uid}`,
+                type: "bezier",
                 source: node.behavior.uid,
                 target: goTo.uid,
             });
         });
     });
-
-    console.log("Generated nodes:", nodes);
-    console.log("Generated edges:", edges); 
 
     return {
         nodes: nodes,
