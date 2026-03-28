098f02cab223a36e5dc5be1870f591076900ac54
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-28 17:01:45 -0400

    Add BehavioralGraphBuilder to exported components.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export * from "./components/StackList";
 export * from "./components/FlowDiagram";
 export * from "./components/FileBrowser";
 export * from "./components/Editor";
+export * from "./components/BehavioralGraphBuilder";
